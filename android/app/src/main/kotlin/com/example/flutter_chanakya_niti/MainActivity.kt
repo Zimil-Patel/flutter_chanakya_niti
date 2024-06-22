e8632318dfede40cd751d25e02b9ce39abaa8e44
Zimil-Patel <patelzimil2310@gmail.com>
2024-06-22 12:49:25 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_chanakya_niti/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_chanakya_niti
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(ChanakyaNitiApp());
+}
+
+
+class ChanakyaNitiApp extends StatelessWidget {
+  const ChanakyaNitiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
